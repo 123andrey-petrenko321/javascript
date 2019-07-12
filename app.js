2df1c4d5f3c21983fda45bf1c3163d36c289233b
123andrey-petrenko321 <48992351+123andrey-petrenko321@users.noreply.github.com>
2019-07-12 17:41:54 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+/*let name;
+let age;
+
+name = prompt ("What is your name?");
+age = prompt("Your age?");
+
+if (Number(age) > 18) {
+   
+    if (Number(age) < 50) {
+        console.log("ok")
+    }
+} else {
+    console.log("!!!!");
+}*/
+/*const color = prompt('enter your color');
+switch (color){
+    case 'green':
+        console.log('apple');
+        break;
+    case 'red':
+        console.log('tomato');
+        break;
+    case 'yellow':
+        console.log('lemone');
+        break;
+    case 'orange':
+        console.log('orange');
+        break;
+
+    default:
+        console.log('----')
+       
+}*/
+
+// let i = 0; 
+// while (i < 10) {
+    // console.log(`i:${i}`);
+    // i++;
+// }
+
+/*for (let i = 0 ;i < 10 ;i++ ) {
+    
+    console.log(`i:${i}`);
+}*/
+
+/*let login = "123", pass = "123";
+do{
+     if(prompt("enter log") == login &&
+     prompt("enter pass")== pass){
+     status = true;
+    }else{
+        alert('wrong')
+    }
+} while(!status)
+alert('ok')*/
+let i= 0;
+while (i < 20) {
+    if(i%2==0 ){
+        console.log(i)
+    }
+    i++;
+}
+
